c9ce5bd1adcdde9f06fed8ae2a82da94dfaa971d
retr0lbb <retr0lbb@gmail.com>
2026-03-03 02:37:51 -0300

    feat: add new method to tenant repository

--- src/modules/tenant/tenant.repository.ts
+++ src/modules/tenant/tenant.repository.ts
@@ -1,11 +1,23 @@
-import {db} from "../../db/index.ts"
-import { tenants } from "../../db/schemas/tenant.ts"
-import type { CreateTenantDTO } from "./tenant.dto.ts"
+import { eq } from "drizzle-orm";
+import { db } from "../../db/index.ts";
+import { tenants } from "../../db/schemas/tenant.ts";
+import type { CreateTenantDTO } from "./tenant.dto.ts";
 
-export class TenantRepository{
-    constructor(){}
-    
-    async create(data: CreateTenantDTO): Promise<void>{
-        await db.insert(tenants).values(data);
-    }
+export class TenantRepository {
+	async create(data: CreateTenantDTO): Promise<void> {
+		await db.insert(tenants).values(data);
+	}
+
+	async findTenantById(tenantId: string) {
+		const tenant = await db
+			.select()
+			.from(tenants)
+			.where(eq(tenants.id, tenantId));
+
+		if (!tenant) {
+			throw new Error("not Found");
+		}
+
+		return tenant;
+	}
 }
